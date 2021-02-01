a93f58343313cc9bead50b8a6e1db6fbe688ee3e
Dongmoon29 <kdm29@naver.com>
2021-02-02 08:59:54 +0900

    Initial project add

---
+++ src/main/java/com/petclinic/kdmclinic/KdmClinicApplication.java
@@ -0,0 +1,13 @@
+package com.petclinic.kdmclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdmClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KdmClinicApplication.class, args);
+    }
+
+}
