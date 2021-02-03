5aef74d3171a6688d9cc10fe7138a63368b1b1fe
Dongmoon29 <kdm29@naver.com>
2021-02-03 10:56:24 +0900

    Refactor interfaces. Closes #10

--- pet-clinic-data/src/main/java/com/petclinic/kdmclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/petclinic/kdmclinic/services/OwnerService.java
@@ -2,11 +2,7 @@ package com.petclinic.kdmclinic.services;
 
 import com.petclinic.kdmclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLastName(String lastName);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
+
 }
--- pet-clinic-data/src/main/java/com/petclinic/kdmclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/petclinic/kdmclinic/services/PetService.java
@@ -2,10 +2,5 @@ package com.petclinic.kdmclinic.services;
 
 import com.petclinic.kdmclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long>{
 }
