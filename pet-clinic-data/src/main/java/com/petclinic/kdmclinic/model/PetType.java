a6eda566a192333fc69ddab6bc27e29f2d028dc9
Dongmoon29 <kdm29@naver.com>
2021-02-24 10:43:55 +0900

    Add project lombok

--- pet-clinic-data/src/main/java/com/petclinic/kdmclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/petclinic/kdmclinic/model/PetType.java
@@ -1,21 +1,33 @@
 package com.petclinic.kdmclinic.model;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="types")
+@Setter
+@Getter
+@NoArgsConstructor
 public class PetType extends BaseEntity {
+
+    @Builder
+    public PetType(Long id, String name) {
+        super(id);
+        this.name = name;
+    }
 
     @Column(name = "name")
     private String name;
 
-    public String getName() {
+    @Override
+    public String toString() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 }
